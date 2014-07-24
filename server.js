413985324277f83ea7039d2a1cf4342429c787be
Federico Raggi <federicoraggi@hotmail.com>
2014-07-24 21:55:55 +0000

    Refactored routes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var slash = require("express-slash");
 var cors = require('cors');
+var errorhandler = require('errorhandler')
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var passport = require('passport');
 var userController = require('./controllers/user');
 var authController = require('./auth/localStrategy');
+
+console.log('Running on: ' + process.env.NODE_ENV);
 
 console.log('Connecting to database');
 var mongodb = process.env.MONGODB || 'localhost';
@@ -20,22 +23,22 @@ mongoose.connect('mongodb://' + mongodb, function(err) {
 console.log('Setting routes');
 var app = express();
 app.enable('strict routing');
-app.enable('case sensitive routing');
 
+console.log('Setting api routes');
 var apiRouter = express.Router({
   caseSensitive: app.get('case sensitive routing'),
   strict       : app.get('strict routing')
 });
-apiRouter.get('/', function(req, res) {
+
+apiRouter.get('/api/', function(req, res) {
   res.json({ message: 'API Root'});
 });
 
-console.log('Setting user routes');
-apiRouter.route('/users')
+apiRouter.route('/api/users')
   .get(userController.list)
   .post(userController.create);
 
-apiRouter.route('/users/:user_id')
+apiRouter.route('/api/users/:user_id')
   .delete(authController.isAuthenticated, userController.remove)
   .get(authController.isAuthenticated, userController.get)
   .put(authController.isAuthenticated, userController.update);
@@ -45,39 +48,44 @@ var clientRouter = express.Router({
   caseSensitive: app.get('case sensitive routing'),
   strict       : app.get('strict routing')
 });
-clientRouter.get('/', function(req, res) {
+
+clientRouter.get('/client/', function(req, res) {
   res.render('index.ejs');
 });
 
-clientRouter.get('/login', function(req, res) {
+clientRouter.get('/client/login', function(req, res) {
   res.render('login.ejs', { message: '' });
 });
 
-clientRouter.get('/signup', function(req, res) {
+clientRouter.get('/client/signup', function(req, res) {
   res.render('signup.ejs', { message: '' });
 });
 
-clientRouter.get('/profile', authController.isAuthenticated, function(req, res) {
+clientRouter.get('/client/profile', authController.isAuthenticated, function(req, res) {
   res.render('profile.ejs', {
     user : req.user
   });
 });
 
-clientRouter.get('/logout', function(req, res) {
+clientRouter.get('/client/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
 
 console.log('Setting middleware');
 app.use(morgan('dev'));
-app.use('/api/', cors());
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use('/api', cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.set('view engine', 'ejs');
-app.use('/api', apiRouter);
-app.use('/client', clientRouter);
+app.use(apiRouter);
+app.use(clientRouter);
 app.use(slash());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorhandler())
+}
 
 // Listen
 var port = process.env.PORT || 8080;
